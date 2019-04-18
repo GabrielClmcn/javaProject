cca1344f5822241f620e14ad12a48a3d3f0980a8
GabrielClmcn <gabriel.clemencon@ynov.com>
2019-04-18 12:55:40 +0200

    add Maven Java Project mysqlconnector/json) and database

---
+++ clientServerBDDJSON/src/com/ynov/clientServerBDDJSON/connector/MySqlConnector.java
@@ -0,0 +1,5 @@
+package com.ynov.clientServerBDDJSON.connector;
+
+public class MySqlConnector {
+
+}
---
+++ clientServerBDDJSON/src/com/ynov/clientServerBDDJSON/launcher/Launcher.java
@@ -0,0 +1,10 @@
+package com.ynov.clientServerBDDJSON.launcher;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
